f0ab38f1b79860bc110f1960279856993d94476a
JeremiCS97 <jeremi.cardenas@pucp.pe>
2020-06-22 19:37:20 -0500

    Simulación

---
+++ src/components/Manager/GestSimulation.vue
@@ -0,0 +1,15 @@
+<template>
+    
+</template>
+
+<script>
+<style src="@/styles/Manager/GetSimulation.css" scoped>
+
+</style>
+import Swal from 'sweetalert2'
+import 'sweetalert2/src/sweetalert2.scss'
+export default {
+    name: 'GetSimulation'
+
+}
+</script>
