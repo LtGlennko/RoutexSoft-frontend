21d2f2f3915fd9ac570f23a7feff03b532d6055b
JeremiCS97 <jeremi.cardenas@pucp.pe>
2020-07-01 14:49:05 -0500

    Cambios de hace 1 semana

--- src/components/Manager/GestRutes.vue
+++ src/components/Manager/GestRutes.vue
@@ -1,5 +1,36 @@
 <template>
-    
+   <v-container>
+        <v-card> 
+            <v-row>
+                <v-col>
+                    <v-card-title class="titleCard">
+                        Reporte Paquetes
+                    </v-card-title>
+                    <v-card-text>
+                        
+                    
+                        <v-data-table   :headers="headers"
+                                        :items="users"
+                                        :items-per-page="5"
+                                        :loading-text="loadingText"
+                                        :no-data-text="noDataText"
+                                        :no-results-text="filterNoResultsText"
+                                        :footer-props="footerProps"
+                                        :search="search"
+                                        class="elevation-1">
+                        </v-data-table>
+                        <v-card-subtitle>
+                            <v-row>
+                                <v-col cols="6">
+                                    <br><v-btn class="mb-2 buttonLoadHistory" @click=LoadHistory()>Cargar Historial</v-btn>
+                                </v-col>
+                            </v-row>
+                        </v-card-subtitle>
+                    </v-card-text>
+                </v-col>
+            </v-row>
+        </v-card>
+    </v-container> 
 </template>
 
 <script>
